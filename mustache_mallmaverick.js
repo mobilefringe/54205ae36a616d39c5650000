bb64a360ce3e5dfe3aa817e00e9965d61ae05bc6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-26 11:05:53 -0400

    Place Vertu 0.9.3  /mustache_mallmaverick.js

--- mustache_mallmaverick.js
+++ mustache_mallmaverick.js
@@ -418,10 +418,11 @@ function renderRegularDayHours(template_id,html_id,day_of_week){
     var day_hours = getRegHoursForDayIndex(day_of_week);
     console.log(day_hours);
     if (day_hours.open_time && day_hours.close_time && day_hours.is_closed == false){
-        var open_time = in_my_time_zone(moment(day_hours.open_time), "h:mm A");
-        var close_time = in_my_time_zone(moment(day_hours.close_time), "h:mm A");
-        day_hours.open_time_localized = open_time;
-        day_hours.close_time_localized = close_time;
+        var open_time = moment(day_hours.open_time).tz(getPropertyTimeZone()); 
+            
+        var close_time = moment(day_hours.close_time).tz(getPropertyTimeZone()); 
+        day_hours.open_time_localized = open_time.format("h:mm A");
+        day_hours.close_time_localized = close_time.format("h:mm A");
     } else {
         "Closed";
     }
