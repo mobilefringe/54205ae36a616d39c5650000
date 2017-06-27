487db0dc2309c0b2102120d346142723b0a5e582
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-27 15:38:01 -0400

    Place Vertu 0.9.3  /mustache_mallmaverick.js

--- mustache_mallmaverick.js
+++ mustache_mallmaverick.js
@@ -502,12 +502,12 @@ function renderHolidayHours(template_id,html_id,not_empty_section_id,empty_secti
             var open_time = moment(val.open_time).tz(getPropertyTimeZone()); 
             var close_time = moment(val.close_time).tz(getPropertyTimeZone()); 
             if(Cookies.get('current_locale') == "fr-CA"){
-                val.name = val.holiady_name_2;
+                val.holiday_name = val.holiady_name_2;
                 val.open_time = open_time.format("H") + "h" + open_time.format("mm");
                 val.close_time = close_time.format("H") + "h" + close_time.format("mm");
             }
             if(Cookies.get('current_locale') == "en-CA"){
-                val.name = val.holiady_name;
+                // val.name = val.holiady_name;
                 val.open_time = open_time.format("h:mm A");
                 val.close_time = close_time.format("h:mm A");
             }
