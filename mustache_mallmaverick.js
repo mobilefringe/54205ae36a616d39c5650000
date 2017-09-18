4d8bd5a1af5f2167b3843a7fc98b2392e27b7483
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-18 14:08:18 -0400

    Place Vertu 0.9.3  /mustache_mallmaverick.js

--- mustache_mallmaverick.js
+++ mustache_mallmaverick.js
@@ -34,7 +34,7 @@ function renderStoresListTemplate(header_template_id,item_template_id,html_id,no
         var item_rendered = Mustache.render(item_template_html,val);
         item_list.push(item_rendered);
     });
-    if(stores.length > 0){
+    // if(stores.length > 0){
     if(stores != null){
         $(not_empty_section_id).show();
         $(empty_section_id).hide();
