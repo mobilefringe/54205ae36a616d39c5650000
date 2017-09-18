e77153950750f09f40efc516d27506b8ba914ae0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-18 15:16:14 -0400

    Place Vertu 0.9.3  /mustache_mallmaverick.js

--- mustache_mallmaverick.js
+++ mustache_mallmaverick.js
@@ -428,67 +428,67 @@ function renderEventDetailsTemplate(template_id,html_id,event_details){
     $(html_id).html(rendered);
 }
 
-// function renderRegularDayHours(template_id,html_id,day_of_week){
-//     var template_html = $(template_id).html();
-//     Mustache.parse(template_html);   // optional, speeds up future uses
-//     var day_hours = getRegHoursForDayIndex(day_of_week);
-    
-//     var open_time = moment(day_hours.open_time).tz(getPropertyTimeZone()); 
-//     var close_time = moment(day_hours.close_time).tz(getPropertyTimeZone()); 
-//     if(Cookies.get('current_locale') == "fr-CA"){
-//         day_hours.open_time = open_time.format("H") + "h";
-//         day_hours.close_time = close_time.format("H") + "h";
-//     }
-//     if(Cookies.get('current_locale') == "en-CA"){
-//         day_hours.open_time = open_time.format("h:mm A");
-//         day_hours.close_time = close_time.format("h:mm A");
-//     }
-
-//     if(day_hours.is_closed){
-//         day_hours.is_open_css = "display:none";
-//         day_hours.is_closed_css = "display:inline";
-//     }else{
-//         day_hours.is_open_css = "display:inline";
-//         day_hours.is_closed_css = "display:none";
-//     }
-//     setLocaleDateFormats(day_hours);
-//     var rendered = Mustache.to_html(template_html,day_hours);
-//     $(html_id).html(rendered);
-// }
-
-function renderRegularDayHours(template_id, html_id, hours){
-    var item_list = [];
+function renderRegularDayHours(template_id,html_id,day_of_week){
     var template_html = $(template_id).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
-    $.each( hours , function( key, val ) {
-        
-        // var day_hours = getRegHoursForDayIndex(day_of_week);
-        
-        var open_time = moment(val.open_time).tz(getPropertyTimeZone()); 
-        var close_time = moment(val.close_time).tz(getPropertyTimeZone()); 
-        if(Cookies.get('current_locale') == "fr-CA"){
-            val.open_time = open_time.format("H") + "h";
-            val.close_time = close_time.format("H") + "h";
-        }
-        if(Cookies.get('current_locale') == "en-CA"){
-            val.open_time = open_time.format("h:mm A");
-            val.close_time = close_time.format("h:mm A");
-        }
+    var day_hours = getRegHoursForDayIndex(day_of_week);
     
-        // if(day_hours.is_closed){
-        //     day_hours.is_open_css = "display:none";
-        //     day_hours.is_closed_css = "display:inline";
-        // }else{
-        //     day_hours.is_open_css = "display:inline";
-        //     day_hours.is_closed_css = "display:none";
-        // }
-       
-        setLocaleDateFormats(hours);
-    });
-    
-    var rendered = Mustache.to_html(template_html, hours);
+    var open_time = moment(day_hours.open_time).tz(getPropertyTimeZone()); 
+    var close_time = moment(day_hours.close_time).tz(getPropertyTimeZone()); 
+    if(Cookies.get('current_locale') == "fr-CA"){
+        day_hours.open_time = open_time.format("H") + "h";
+        day_hours.close_time = close_time.format("H") + "h";
+    }
+    if(Cookies.get('current_locale') == "en-CA"){
+        day_hours.open_time = open_time.format("h:mm A");
+        day_hours.close_time = close_time.format("h:mm A");
+    }
+
+    if(day_hours.is_closed){
+        day_hours.is_open_css = "display:none";
+        day_hours.is_closed_css = "display:inline";
+    }else{
+        day_hours.is_open_css = "display:inline";
+        day_hours.is_closed_css = "display:none";
+    }
+    setLocaleDateFormats(day_hours);
+    var rendered = Mustache.to_html(template_html,day_hours);
     $(html_id).html(rendered);
 }
+
+// function renderRegularDayHours(template_id, html_id, hours){
+//     var item_list = [];
+//     var template_html = $(template_id).html();
+//     Mustache.parse(template_html);   // optional, speeds up future uses
+//     $.each( hours , function( key, val ) {
+        
+//         // var day_hours = getRegHoursForDayIndex(day_of_week);
+        
+//         var open_time = moment(val.open_time).tz(getPropertyTimeZone()); 
+//         var close_time = moment(val.close_time).tz(getPropertyTimeZone()); 
+//         if(Cookies.get('current_locale') == "fr-CA"){
+//             val.open_time = open_time.format("H") + "h";
+//             val.close_time = close_time.format("H") + "h";
+//         }
+//         if(Cookies.get('current_locale') == "en-CA"){
+//             val.open_time = open_time.format("h:mm A");
+//             val.close_time = close_time.format("h:mm A");
+//         }
+    
+//         // if(day_hours.is_closed){
+//         //     day_hours.is_open_css = "display:none";
+//         //     day_hours.is_closed_css = "display:inline";
+//         // }else{
+//         //     day_hours.is_open_css = "display:inline";
+//         //     day_hours.is_closed_css = "display:none";
+//         // }
+       
+//         setLocaleDateFormats(hours);
+//     });
+    
+//     var rendered = Mustache.to_html(template_html, hours);
+//     $(html_id).html(rendered);
+// }
 
 function renderTodaysHours(template_id,html_id){
     var template_html = $(template_id).html();
